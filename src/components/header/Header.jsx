314370efa4464231999018a76562777dbaf1a6bd
Ella-Kadriaieva <ella.kadriaieva@gmail.com>
2025-03-01 01:57:58 +0200

    create isMobile and refactor code

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,35 @@
 import React from "react";
-import { Link } from "react-router";
+import { Menu } from "lucide-react";
 import Container from "../container/Container";
 import NavMenu from "../navMenu/NavMenu";
 import ButtonLink from "../ui/ButtonLink";
-import styles from "./Header.module.scss";
 import IconsBlockHeader from "../iconsBlockHeader/IconsBlockHeader";
 import LogoThemeBlock from "../logoThemeBlock/LogoThemeBlock";
+import styles from "./Header.module.scss";
+import { useModal } from "../../context/ModalContext";
 export default function Header() {
+  const { isMobile } = useModal();
   return (
     <header>
       <Container>
         <div className={styles.headerWrapper}>
           <LogoThemeBlock />
-          <div className={styles.navWrapper}>
-            <ButtonLink
-              to="/categories" //НУЖНО ВЫЯСНИТЬ КУДА ПЕРЕБРАСЫВАЕТ
-              text="1 day discount"
-              className={styles.discountBtn}
-              type="link"
-            />
-            <NavMenu />
+          {!isMobile && (
+            <div className={styles.navWrapper}>
+              <ButtonLink
+                to="/categories" //НУЖНО ВЫЯСНИТЬ КУДА ПЕРЕБРАСЫВАЕТ
+                text="1 day discount"
+                className={styles.discountBtn}
+                type="link"
+              />
+              <NavMenu />
+            </div>
+          )}
+
+          <div className={styles.icons}>
+            <IconsBlockHeader />
+            {isMobile && <Menu className={styles.svgBtn} />}
           </div>
-          <IconsBlockHeader />
         </div>
       </Container>
     </header>
--- src/context/ModalContext.jsx
+++ src/context/ModalContext.jsx
@@ -2,10 +2,10 @@ import React, { createContext, useState, useContext, useEffect } from "react";
 const ModalContext = createContext();
 export const ModalProvider = ({ children }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 480);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 767.98);
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 480);
+      setIsMobile(window.innerWidth <= 767.98);
     };
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
