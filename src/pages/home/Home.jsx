697f7f88464cedea6509633cc88ce549228ffcba
Mykhailo Hubko <mixelios@gmail.com>
2025-03-03 12:54:03 +0100

    section title

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,19 @@ import {Candy} from "lucide-react";
 
 import React from "react";
 import Exemple from "../../components/exemple/Exemple";
+import SectionTitle from "../../components/sectionTitle/sectionTitle";
+import Container from "../../components/container/Container";
 
 export default function Home() {
 
   return (
     <div>
       <h1>Hero</h1>
+      <Container>
+      <SectionTitle>
+        Categories
+      </SectionTitle>
+      </Container>
       <h2>Categories Section</h2>
       <h2>Form Section</h2>
       <h2>Product Section</h2>
