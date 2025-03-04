4180f5fb1393c614899ca99637f245d9c2b9cf1d
Ella-Kadriaieva <ella.kadriaieva@gmail.com>
2025-03-04 21:25:42 +0200

    delete css extra files

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,21 +1,17 @@
-import {Candy} from "lucide-react";
+import { Candy } from 'lucide-react';
 
-import React from "react";
-import SectionTitle from "../../components/sectionTitle/sectionTitle";
-import Container from "../../components/container/Container";
-import MainSection from "../../components/mainSection/MainSection";
+import React from 'react';
+import SectionTitle from '../../components/sectionTitle/sectionTitle';
+import Container from '../../components/container/Container';
+import MainSection from '../../components/mainSection/MainSection';
 
 export default function Home() {
-
   return (
     <>
-     <MainSection />
+      <MainSection />
       <Container>
         <SectionTitle>Categories</SectionTitle>
       </Container>
-      <h2>Categories Section</h2>
-      <h2>Form Section</h2>
-      <h2>Product Section</h2>
     </>
   );
 }
