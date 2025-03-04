6fab228eda3adb8ba6b4775597cebb4438972524
Ella-Kadriaieva <ella.kadriaieva@gmail.com>
2025-03-04 23:43:54 +0200

    fix changes

--- src/components/saleSection/SaleSection.jsx
+++ src/components/saleSection/SaleSection.jsx
@@ -1,58 +1,34 @@
-
-import Container from "../container/Container"
-import SaleCard from '../saleCard/SaleCard'
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  errorProductRequest,
-  startProductRequest,
-  finishProductRequest
-} from "../../store/slices/productSlice";
-import { getAllProducts } from "../../utils/fetchClient";
-import styles from "./SaleSection.module.scss"
-
+import useFetchProducts from '../../utils/useFetchProducts';
+import Container from '../container/Container';
+import SaleCard from '../saleCard/SaleCard';
+import styles from './SaleSection.module.scss';
+import getRandomElements from '../../utils/randomMain';
 const SaleSection = () => {
-  const products = useSelector((state) => state.products);
-  const { data, loading, error } = products;
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const requestProducts = async () => {
-      try {
-        dispatch(startProductRequest());
-        const data = await getAllProducts();
-        dispatch(finishProductRequest(data));
-      } catch (err) {
-        dispatch(errorProductRequest(err.massage || err));
-      }
-    };
-    requestProducts();
-  }, []);
+  const { data, loading, error } = useFetchProducts('sale');
+  const randomCards = getRandomElements(data);
 
   return (
-  <section>
-    <Container>
-      <div className={styles.cardsContainer}>
-    {loading && <div>Loading...</div>}
-      {products &&
-        // data.length > 0 &&
-        // products?.data.slice(0,4).map((item) => <SaleCard key={item.id} price={item.price} title={item.title}  image={item.image} percent={item.discont_price}/>)}
-        data.filter((item) => item.discont_price > 0).slice(0, 4).map((item) => (
-        <SaleCard
-          key={item.id}
-          price={item.price}
-          title={item.title}
-          image={item.image}
-          percent={item.discont_price} /> ))}
-        
-      </div>
-    </Container>
-  </section>
-  )
-}
+    <section>
+      <Container>
+        <div className={styles.cardsContainer}>
+          {error && <div>Error fetching data</div>}
+          {loading && <div>Loading...</div>}
+          {data &&
+            randomCards
+              .slice(0, 4)
+              .map((item) => (
+                <SaleCard
+                  key={item.id}
+                  price={item.price}
+                  title={item.title}
+                  image={item.image}
+                  percent={item.discont_price}
+                />
+              ))}
+        </div>
+      </Container>
+    </section>
+  );
+};
 
-export default SaleSection
-
-
-
+export default SaleSection;
