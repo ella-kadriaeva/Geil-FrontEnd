954caf13193686a83e40538319ae45512b00ba32
Mykhailo Hubko <mixelios@gmail.com>
2025-03-04 10:45:41 +0100

    empty cart done

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -7,26 +7,31 @@ export default function Cart() {
   return (
     <section className={styles.cart}>
       <Container>
-        <div className={styles.cart_titleBlock}>
-          <h2 className={styles.cart_title}>
-            Shopping cart
-          </h2>
+        <div className={`${styles.cart_titleBlock}`}>
+          <h2 className={styles.cart_title}>Shopping cart</h2>
           <span className={styles.cart_titleLine}></span>
           <ButtonLink
             to="/products"
             text="Back to the store"
-            className={styles.cart_button__outlined}
+            className={`${styles.cart_button__outlined} ${styles.cart_button__outlined_inTitle}`}
           />
         </div>
 
-        <div className="cart_empty">
-          <p className={styles.cart_emptyInfo}>Looks like you have no items in your basket currently.</p>
+        <div className={styles.cart_empty}>
+          <p className={styles.cart_emptyInfo}>
+            Looks like you have no items in your basket currently.
+          </p>
           <ButtonLink
             to="/products"
             text="Continue Shopping"
             className={styles.cart_button}
           />
         </div>
+        <ButtonLink
+          to="/products"
+          text="Back to the store"
+          className={`${styles.cart_button__outlined} ${styles.cart_button__outlined_outside}`}
+        />
       </Container>
     </section>
   );
