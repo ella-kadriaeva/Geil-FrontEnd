389b448bda5c2c53f0362566b4d4cc1a562b47e8
Ella-Kadriaieva <ella.kadriaieva@gmail.com>
2025-03-04 19:30:13 +0200

    merge changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import {Candy} from "lucide-react";
 
 import React from "react";
-import Exemple from "../../components/exemple/Exemple";
+
 
 export default function Home() {
 
@@ -11,8 +11,7 @@ export default function Home() {
       <h2>Categories Section</h2>
       <h2>Form Section</h2>
       <h2>Product Section</h2>
-      <Exemple />
-      <Candy size="64" color="violet" />
+    
     </div>
   );
 }
