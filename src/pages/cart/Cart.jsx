525ae9109363876cf627e2619ee3dfc433cd8dde
Ella-Kadriaieva <ella.kadriaieva@gmail.com>
2025-03-09 17:55:11 +0200

    create cartSlice and function add to cart

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,9 +1,16 @@
-import styles from "./Cart.module.scss"
-import ButtonLink from "../../components/ui/ButtonLink";
-import Container from "../../components/container/Container";
+import styles from './Cart.module.scss';
+import ButtonLink from '../../components/ui/ButtonLink';
+import Container from '../../components/container/Container';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { initDataFromLocalStorage } from '../../store/slices/cartSlice';
 
 export default function Cart() {
-
+  const dispatch = useDispatch();
+  const { cartData } = useSelector((state) => state.cart);
+  useEffect(() => {
+    dispatch(initDataFromLocalStorage());
+  }, []);
   return (
     <section className={styles.cart}>
       <Container>
@@ -17,16 +24,19 @@ export default function Cart() {
           />
         </div>
 
-        <div className={styles.cart_empty}>
-          <p className={styles.cart_emptyInfo}>
-            Looks like you have no items in your basket currently.
-          </p>
-          <ButtonLink
-            to="/products"
-            text="Continue Shopping"
-            className={styles.cart_button}
-          />
-        </div>
+        {!cartData && (
+          <div className={styles.cart_empty}>
+            <p className={styles.cart_emptyInfo}>
+              Looks like you have no items in your basket currently.
+            </p>
+            <ButtonLink
+              to="/products"
+              text="Continue Shopping"
+              className={styles.cart_button}
+            />
+          </div>
+        )}
+        {cartData && cartData.map((item) => <div key={item.id}>{item.id}</div>)}
         <ButtonLink
           to="/products"
           text="Back to the store"
