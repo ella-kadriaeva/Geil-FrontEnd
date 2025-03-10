fb88eef185111c2a276e5aaaf50bb34a0c2926f4
irina211093 <irinadanilkor@gmail.com>
2025-03-10 11:13:07 +0100

    fixed countBtn, added function Readme

--- src/components/saleCard/SaleCard.jsx
+++ src/components/saleCard/SaleCard.jsx
@@ -1,11 +1,16 @@
+
 import React from 'react';
 import styles from './SaleCard.module.scss';
 
 const BASE_URL = 'http://localhost:3333';
 
 const SaleCard = ({ title, image, percent, price }) => {
-  const discontPrice = Number(((price * percent) / 100).toFixed(2));
-  const actualPrice = percent > 0 ? discontPrice : Number(price.toFixed(2));
+  // Вычисляем скидку, чтобы вычесть ее из исходной цены
+  const discountAmount = (price * percent) / 100;
+  const discountPrice = price - discountAmount;  // Скидочная цена
+
+  // Если скидка есть, то показываем цену со скидкой, иначе обычную цену
+  const actualPrice = percent > 0 ? discountPrice : price;
 
   return (
     <div className={styles.productCard}>
@@ -24,8 +29,14 @@ const SaleCard = ({ title, image, percent, price }) => {
       <div className={styles.productCard_text}>
         <h3>{title}</h3>
         <div className={styles.productCard_text_priceBlock}>
-          <p className={styles.productCard_text_actualPrice}>${actualPrice}</p>
-          {percent && <p className={styles.productCard_text_price}>${price}</p>}
+          <p className={styles.productCard_text_actualPrice}>
+            ${actualPrice.toFixed(2)} {/* Показываем цену со скидкой */}
+          </p>
+          {percent && (
+            <p className={styles.productCard_text_price}>
+              ${price.toFixed(2)} {/* Показываем исходную цену */}
+            </p>
+          )}
         </div>
       </div>
     </div>
@@ -33,3 +44,4 @@ const SaleCard = ({ title, image, percent, price }) => {
 };
 
 export default SaleCard;
+
