5d0dc95da260495d5ce74b562994daf21d34fe0b
Mykhailo Hubko <mixelios@gmail.com>
2025-03-16 20:50:38 +0100

    adaptive cart completed

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,10 @@ import Sales from './pages/sales/Sales';
 import NotFound from './pages/notFound/NotFound';
 import Cart from './pages/cart/Cart';
 import Likes from './pages/likes/Likes';
-import ProductDetails from './pages/productDetails/ProductDetails';
 import { useModal } from './context/ModalContext';
 import './App.scss';
+import ProductDetailsSection from './components/productDetailsSection/ProductDetailsSection';
+
 function App() {
   const { isMobile } = useModal();
   return (
@@ -24,7 +25,7 @@ function App() {
         <Route path="categories" element={<Categories />} />
         <Route path="/categories/:id" element={<CategoryProducts />} />
         <Route path="/products" element={<Products />} />
-        <Route path="/products/:productId" element={<ProductDetails />} />
+        <Route path="/products/:productId" element={<ProductDetailsSection />} />
         <Route path="/sale" element={<Sales />} />
         <Route path="cart" element={<Cart />} />
         <Route path="likes" element={<Likes />} />
